6114369595a4bc8c275ac96ab93f80267d770a61
BM2134-m <smkr.3248@gmail.com>
2025-07-10 10:53:28 +0530

    Update Ridham.sql

--- Ridham.sql
+++ Ridham.sql
@@ -1,2 +1,7 @@
 Emp_Data = pd.DataFreame(States, Education,asxi =1)
-
+ipmort numpy as np
+row_size = 9
+City = ['Mumbai','Pune','Satara']
+City_Name =np.repeat(City,repeats=row_size/len(City))
+np.random.shuffle(City_Name)
+City_Name
